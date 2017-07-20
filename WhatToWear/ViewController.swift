f3d34efa74181ce7192c675b07c9da0bc17a5c14
NaixVlad <Naixxx@icloud.com>
2017-07-20 14:46:33 +0300

    Initial Commit

---
+++ WhatToWear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatToWear
+//
+//  Created by Vladislav Andreev on 20.07.17.
+//  Copyright © 2017 Vladislav Andreev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

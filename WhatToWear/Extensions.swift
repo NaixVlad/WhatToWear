4cf33e054db4eec6efa6e53455abf053665d03f3
NaixVlad <Naixxx@icloud.com>
2017-09-02 01:14:50 +0300

    change extensions

--- WhatToWear/Extensions.swift
+++ WhatToWear/Extensions.swift
@@ -128,27 +128,27 @@ extension Date {
     func years(from date: Date) -> Int {
         return Calendar.current.dateComponents([.year], from: date, to: self).year ?? 0
     }
-    /// Returns the amount of months from another date
+    
     func months(from date: Date) -> Int {
         return Calendar.current.dateComponents([.month], from: date, to: self).month ?? 0
     }
-    /// Returns the amount of weeks from another date
+   
     func weeks(from date: Date) -> Int {
         return Calendar.current.dateComponents([.weekOfMonth], from: date, to: self).weekOfMonth ?? 0
     }
-    /// Returns the amount of days from another date
+    
     func days(from date: Date) -> Int {
         return Calendar.current.dateComponents([.day], from: date, to: self).day ?? 0
     }
-    /// Returns the amount of hours from another date
+    
     func hours(from date: Date) -> Int {
         return Calendar.current.dateComponents([.hour], from: date, to: self).hour ?? 0
     }
-    /// Returns the amount of minutes from another date
+    
     func minutes(from date: Date) -> Int {
         return Calendar.current.dateComponents([.minute], from: date, to: self).minute ?? 0
     }
-    /// Returns the amount of seconds from another date
+    
     func seconds(from date: Date) -> Int {
         return Calendar.current.dateComponents([.second], from: date, to: self).second ?? 0
     }
@@ -178,7 +178,6 @@ extension Date {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE"
         return dateFormatter.string(from: self).capitalized
-        // or use capitalized(with: locale) if you want
     }
     
     func getHour() -> String? {
@@ -234,37 +233,6 @@ extension DataBlock {
         return days
         
     }
-    
-    /*func getDates() -> [[Date]] {
-        
-        let dataPoints = self.data
-        
-        var days = [[Date]]()
-        
-        var dayFlag = 0
-        let midnight = "0:0"
-        
-        for i in 0..<dataPoints.count {
-            
-            let time = dataPoints[i].time.getTime()
-            
-            if (time == midnight && dayFlag != i) {
-                
-                let daySlice = dataPoints[dayFlag..<i+1]
-                let dayDataPoint = Array(daySlice)
-                let dayDates = dayDataPoint.map({ (dataPoint: DataPoint) -> Date in
-                    dataPoint.time
-                })
-                days.append(dayDates)
-                dayFlag = i
-                
-            }
-            
-        }
-        
-        return days
-        
-    }*/
     
     func getDates() -> [[Date]] {
         
@@ -353,9 +321,3 @@ extension DataBlock {
         
     }
 }
-
-
-extension Notification.Name {
-    static let locationChanged = Notification.Name("locationChanged")
-}
-
